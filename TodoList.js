80a1ee7a8ffcbf55627d0aa6d7015abd1638a71d
fran <43730303+frandamore11@users.noreply.github.com>
2024-08-25 18:51:54 -0600

    Add files via upload

---
+++ TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default class TodoList extends React.Component {
+  render() {
+
+    const {todos, toggleCompletion} = this.props
+    return (
+      <div>
+        {
+          todos.map((todo) => (<Todo key={todo.id} todo={todo} toggleCompletion={toggleCompletion}/>))
+      }
+      </div>
+    )
+  }
+}
